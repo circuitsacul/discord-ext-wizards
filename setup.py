0dae9bf0969bfece802fd93a77077903b55ced32
CircuitSacul <55721663+CircuitSacul@users.noreply.github.com>
2021-05-20 17:48:21 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
     license="MIT",
     description="A module for making setup wizards easily.",
     long_description=README,
+    long_description_content_type="text/markdown",
     install_requires=["discord.py>=1.2.5"],
     python_requires=">=3.5.3",
 )
