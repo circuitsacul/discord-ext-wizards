6338ce0231861ea4ea442f93e8480d6b2c1e1ac2
CircuitSacul <55721663+CircuitSacul@users.noreply.github.com>
2021-05-20 17:39:39 -0400

    change version, add readme to long description

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
+import pathlib
 from setuptools import setup
 
 from discord.ext.wizards import __version__
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
 
 setup(
     name="discord-ext-wizards",
@@ -10,6 +14,7 @@ setup(
     packages=["discord.ext.wizards"],
     license="MIT",
     description="A module for making setup wizards easily.",
+    long_description=README,
     install_request=["discord.py>=1.2.5"],
     python_requires=">=3.5.3",
 )
